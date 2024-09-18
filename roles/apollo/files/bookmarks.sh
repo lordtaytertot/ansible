50302882acb0b17a9b2b2371678541f3493c156b
Lord Taytertot <jamestaylor0213@gmail.com>
2024-09-18 07:58:46 -0400

    install bookmarks.sh

---
+++ roles/apollo/files/bookmarks.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+
+PERSONAL_PROFILE=Default
+BUSINESS_PROFILE="Profile 1"
+
+PERSONAL_SOURCE_FILE=$( mktemp )
+PERSONAL_APOLLO_PATH=$( mktemp -p /mnt/zeus/Downloads/ )
+PERSONAL_ZEUS_PATH=/mnt/raid/Downloads/$( echo ${PERSONAL_APOLLO_PATH} | awk -F '/' '{ print $5 }')
+
+BUSINESS_APOLLO_PATH=$( mktemp -p /mnt/zeus/Downloads/ )
+BUSINESS_ZEUS_PATH=/mnt/raid/Downloads/$( echo ${BUSINESS_APOLLO_PATH} | awk -F '/' '{ print $5 }')
+
+cp "${HOME}/.config/google-chrome/${PERSONAL_PROFILE}/Bookmarks" ${PERSONAL_SOURCE_FILE}
+jq 'del(.roots.bookmark_bar.children[0].children[0,1,2,5],.roots.bookmark_bar.children[33:])' ${PERSONAL_SOURCE_FILE} > ${PERSONAL_APOLLO_PATH}
+ssh zeus "rclone copyto ${PERSONAL_ZEUS_PATH} onedrive:/bookmarks/personal.json"
+
+cp "${HOME}/.config/google-chrome/${BUSINESS_PROFILE}/Bookmarks" ${BUSINESS_APOLLO_PATH}
+ssh zeus "rclone copyto ${BUSINESS_ZEUS_PATH} onedrive:/bookmarks/business.json"
