ba6f333a6b5f52c7103886d76790a1e8ca5b4c19
Lord Taytertot <jamestaylor0213@gmail.com>
2024-09-19 21:32:51 -0400

    remove chrome://apps

--- roles/apollo/files/bookmarks.sh
+++ roles/apollo/files/bookmarks.sh
@@ -7,6 +7,7 @@ PERSONAL_SOURCE_FILE=$( mktemp )
 PERSONAL_APOLLO_PATH=$( mktemp -p /mnt/zeus/Downloads/ )
 PERSONAL_ZEUS_PATH=/mnt/raid/Downloads/$( echo ${PERSONAL_APOLLO_PATH} | awk -F '/' '{ print $5 }')
 
+BUSINESS_SOURCE_FILE=$( mktemp )
 BUSINESS_APOLLO_PATH=$( mktemp -p /mnt/zeus/Downloads/ )
 BUSINESS_ZEUS_PATH=/mnt/raid/Downloads/$( echo ${BUSINESS_APOLLO_PATH} | awk -F '/' '{ print $5 }')
 
@@ -14,5 +15,6 @@ cp "${HOME}/.config/google-chrome/${PERSONAL_PROFILE}/Bookmarks" ${PERSONAL_SOUR
 jq 'del(.roots.bookmark_bar.children[0].children[0,1,2,5],.roots.bookmark_bar.children[33:])' ${PERSONAL_SOURCE_FILE} > ${PERSONAL_APOLLO_PATH}
 ssh zeus "rclone copyto ${PERSONAL_ZEUS_PATH} onedrive:/bookmarks/personal.json"
 
-cp "${HOME}/.config/google-chrome/${BUSINESS_PROFILE}/Bookmarks" ${BUSINESS_APOLLO_PATH}
+cp "${HOME}/.config/google-chrome/${BUSINESS_PROFILE}/Bookmarks" ${BUSINESS_SOURCE_FILE}
+jq 'del(.roots.bookmark_bar.children[0])' $BUSINESS_SOURCE_FILE > $BUSINESS_APOLLO_PATH
 ssh zeus "rclone copyto ${BUSINESS_ZEUS_PATH} onedrive:/bookmarks/business.json"
