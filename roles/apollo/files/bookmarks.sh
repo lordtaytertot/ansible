08797d53f3cbbbc0d91491bd62d1823d3ef6eb01
Lord Taytertot <jamestaylor0213@gmail.com>
2024-10-28 15:54:42 -0400

    update bookmarks.sh

--- roles/apollo/files/bookmarks.sh
+++ roles/apollo/files/bookmarks.sh
@@ -12,7 +12,7 @@ BUSINESS_APOLLO_PATH=$( mktemp -p /mnt/zeus/Downloads/ )
 BUSINESS_ZEUS_PATH=/mnt/raid/Downloads/$( echo ${BUSINESS_APOLLO_PATH} | awk -F '/' '{ print $5 }')
 
 cp "${HOME}/.config/google-chrome/${PERSONAL_PROFILE}/Bookmarks" ${PERSONAL_SOURCE_FILE}
-jq 'del(.roots.bookmark_bar.children[0].children[0,1,2,5],.roots.bookmark_bar.children[26:],.sync_metadata)' ${PERSONAL_SOURCE_FILE} > ${PERSONAL_APOLLO_PATH}
+jq 'del(.roots.bookmark_bar.children[0].children[0,1,2,5],.roots.bookmark_bar.children[25:],.sync_metadata)' ${PERSONAL_SOURCE_FILE} > ${PERSONAL_APOLLO_PATH}
 ssh zeus "rclone copyto ${PERSONAL_ZEUS_PATH} onedrive:/bookmarks/personal/Bookmarks && rm ${PERSONAL_ZEUS_PATH}"
 
 cp "${HOME}/.config/google-chrome/${BUSINESS_PROFILE}/Bookmarks" ${BUSINESS_SOURCE_FILE}
